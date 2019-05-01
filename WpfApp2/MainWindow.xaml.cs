a816ca9f91a9d36f5c8a0693498979f39e603cf8
Sidsand <dshordg@gmail.com>
2019-05-01 22:58:29 +0700

    V 1.2 (server)

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
 
 namespace WpfApp2
 {
@@ -20,13 +26,72 @@ namespace WpfApp2
     /// </summary>
     public partial class MainWindow : Window
     {
-        const int port = 666;
+        const int port = 55555;
         const string address = "127.0.0.1";
-        
+        TcpClient client = null;
+        NetworkStream stream;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void Enter_Click(object sender, RoutedEventArgs e)
+        {
+            
+            try
+            {
+                if (name != null)
+                {
+                    string nam = name.Text;
+                    string mes = message.Text;
+                    dialog.Items.Add(name + ": " + mes);
+
+                    byte[] data = Encoding.Unicode.GetBytes(mes);
+                    //  stream.Write(data, 0, data.Length);
+                }
+            }
+            catch { MessageBox.Show("Пожалуйста, введите имя"); }
+        }
+
+        private void Connect_Click(object sender, RoutedEventArgs e)
+        {
+            client = new TcpClient(address, port);
+            stream = client.GetStream();
+
+            Thread myThread1 = new Thread(new ThreadStart(Victory));
+            myThread1.Start();
+        }
+
+        public void Victory()
+        {
+            while (true)
+            {
+                byte[] data = new byte[64];
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0;
+                do
+                {
+                    bytes = stream.Read(data, 0, data.Length);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (stream.DataAvailable);
+
+                string mes = builder.ToString();
+
+                Dispatcher.BeginInvoke(new Action(() => dialog.Items.Add("server: " + mes)));
+            }
+        }
+
+        
+
+        private void Dconnect_Click_1(object sender, RoutedEventArgs e)
+        {
+             string mes = "Отключился";
+            byte[] data = Encoding.Unicode.GetBytes(mes);
+            stream.Write(data, 0, data.Length);
+            stream.Close();
+            client.Close();
+        }
     }
 }
